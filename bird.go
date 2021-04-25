180b074f1a68254b5757101a1937292f494c05e9
Ben_Hsu <ben_hsu@trendmicro.com>
2021-04-26 01:56:21 +0800

    Import fish module from git

--- bird.go
+++ bird.go
@@ -1,9 +1,10 @@
-// tutor.go
 package bird
 
 import "fmt"
+import "github.com/benice7029/fish"
 
 func Bark() string {
 	fmt.Println("Bird chu chu!")
+	fish.Action()
 	return "Bird chu chu!"
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/benice7029/bird
 
 go 1.15
+
+require github.com/benice7029/fish v1.0.1
